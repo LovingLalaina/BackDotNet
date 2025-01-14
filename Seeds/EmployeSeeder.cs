9605b31c54de13d9831b60b483a4691ccdf8c775
Loving <lovinglalainaa@gmail.com>
2025-01-15 01:41:44 +0300

    seeder removed

--- Seeds/EmployeSeeder.cs
+++ Seeds/EmployeSeeder.cs
@@ -13,156 +13,156 @@ namespace back_dotnet.Seeds
     {
         public static async Task SeedEmployeAsync(HairunSiContext context)
         {
-            try
-            {
-                var jsonData = await System.IO.File.ReadAllTextAsync("seeds-id.json");
-                var seedIds = JsonSerializer.Deserialize<SeedData>(jsonData);
+            // try
+            // {
+            //     var jsonData = await System.IO.File.ReadAllTextAsync("seeds-id.json");
+            //     var seedIds = JsonSerializer.Deserialize<SeedData>(jsonData);
 
-                if (seedIds?.Id == null || !seedIds.Id.Any())
-                {
-                    throw new Exception("Les données dans le seeds-id.json son introuvables.");
-                }
+            //     if (seedIds?.Id == null || !seedIds.Id.Any())
+            //     {
+            //         throw new Exception("Les données dans le seeds-id.json son introuvables.");
+            //     }
 
-                var employeId = Guid.Parse(seedIds.Id[1]);
+            //     var employeId = Guid.Parse(seedIds.Id[1]);
 
-                var userRepository = context.Users;
-                var user = await userRepository.FirstOrDefaultAsync(u => u.Email == "test-simple-user@hairun-technology.com");
+            //     var userRepository = context.Users;
+            //     var user = await userRepository.FirstOrDefaultAsync(u => u.Email == "test-simple-user@hairun-technology.com");
 
-                if (user == null)
-                {
-                    var permissionRepository = context.Permissions;
-                    var permissions = new[]
-                    {
-                        await GetOrCreatePermission(permissionRepository, "Accès utilisateur"),
-                        await GetOrCreatePermission(permissionRepository, "Modification utilisateur"),
-                    };
+            //     if (user == null)
+            //     {
+            //         var permissionRepository = context.Permissions;
+            //         var permissions = new[]
+            //         {
+            //             await GetOrCreatePermission(permissionRepository, "Accès utilisateur"),
+            //             await GetOrCreatePermission(permissionRepository, "Modification utilisateur"),
+            //         };
 
-                    var roleRepository = context.Roles;
-                    var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Employé");
+            //         var roleRepository = context.Roles;
+            //         var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Employé");
 
-                    if (role == null)
-                    {
-                        role = new Role
-                        {
-                            Name = "Employé",
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            PermissionRoles = new List<PermissionRole>()
-                        };
+            //         if (role == null)
+            //         {
+            //             role = new Role
+            //             {
+            //                 Name = "Employé",
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 PermissionRoles = new List<PermissionRole>()
+            //             };
 
-                        foreach (var permission in permissions)
-                        {
-                            var permissionRole = new PermissionRole
-                            {
-                                IdPermission = permission.Id,
-                                IdRole = role.Id,
-                                CreatedAt = GetDateTimeNowWithoutTimezone(),
-                                UpdatedAt = GetDateTimeNowWithoutTimezone()
-                            };
+            //             foreach (var permission in permissions)
+            //             {
+            //                 var permissionRole = new PermissionRole
+            //                 {
+            //                     IdPermission = permission.Id,
+            //                     IdRole = role.Id,
+            //                     CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                     UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //                 };
 
-                            role.PermissionRoles.Add(permissionRole);
-                        }
+            //                 role.PermissionRoles.Add(permissionRole);
+            //             }
 
-                        await roleRepository.AddAsync(role);
-                        await context.SaveChangesAsync();
-                    }
+            //             await roleRepository.AddAsync(role);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var departmentRepository = context.Departments;
-                    var department = await departmentRepository.FirstOrDefaultAsync(d => d.Name == "Production");
+            //         var departmentRepository = context.Departments;
+            //         var department = await departmentRepository.FirstOrDefaultAsync(d => d.Name == "Production");
 
-                    if (department == null)
-                    {
-                        department = new Department
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Production",
-                            IdRoleNavigation = role,
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            UpdatedAt = GetDateTimeNowWithoutTimezone()
-                        };
-                        await departmentRepository.AddAsync(department);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (department == null)
+            //         {
+            //             department = new Department
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "Production",
+            //                 IdRoleNavigation = role,
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //             };
+            //             await departmentRepository.AddAsync(department);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var postRepository = context.Posts;
-                    var post = await postRepository.FirstOrDefaultAsync(p => p.Name == "Intégrateur");
+            //         var postRepository = context.Posts;
+            //         var post = await postRepository.FirstOrDefaultAsync(p => p.Name == "Intégrateur");
 
-                    if (post == null)
-                    {
-                        post = new Post
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Intégrateur",
-                            IdDepartment = department.Id,
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            UpdatedAt = GetDateTimeNowWithoutTimezone()
-                        };
-                        await postRepository.AddAsync(post);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (post == null)
+            //         {
+            //             post = new Post
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "Intégrateur",
+            //                 IdDepartment = department.Id,
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //             };
+            //             await postRepository.AddAsync(post);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var fileRepository = context.Files;
-                    var file = await fileRepository.FirstOrDefaultAsync(f => f.Name == "hairun_simple_user");
+            //         var fileRepository = context.Files;
+            //         var file = await fileRepository.FirstOrDefaultAsync(f => f.Name == "hairun_simple_user");
 
-                    if (file == null)
-                    {
-                        file = new Models.Domain.File
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "hairun_simple_user",
-                            Path = "https://res.cloudinary.com/daeuzi9pb/image/upload/v1736163619/uploads/profil_qsh9sh.jpg",
-                            PublicId = "id_simple_user",
-                            Size = "160",
-                            Type = "png"
-                        };
-                        await fileRepository.AddAsync(file);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (file == null)
+            //         {
+            //             file = new Models.Domain.File
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "hairun_simple_user",
+            //                 Path = "https://res.cloudinary.com/daeuzi9pb/image/upload/v1736163619/uploads/profil_qsh9sh.jpg",
+            //                 PublicId = "id_simple_user",
+            //                 Size = "160",
+            //                 Type = "png"
+            //             };
+            //             await fileRepository.AddAsync(file);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var password = BCrypt.Net.BCrypt.HashPassword("SimpleUser@2024*");
+            //         var password = BCrypt.Net.BCrypt.HashPassword("SimpleUser@2024*");
 
-                    var newUser = new User
-                    {
-                        Uuid = employeId,
-                        Firstname = "hairun si user",
-                        Lastname = "hairun si user",
-                        BirthDate = GetDateTimeNowWithoutTimezone(),
-                        Email = "test-simple-user@hairun-technology.com",
-                        Password = password,
-                        IdPost = post.Id,
-                        IdFile = file.Id,
-                        CreatedAt = GetDateTimeNowWithoutTimezone(),
-                        UpdatedAt = GetDateTimeNowWithoutTimezone()
-                    };
-                    await userRepository.AddAsync(newUser);
-                    await context.SaveChangesAsync();
+            //         var newUser = new User
+            //         {
+            //             Uuid = employeId,
+            //             Firstname = "hairun si user",
+            //             Lastname = "hairun si user",
+            //             BirthDate = GetDateTimeNowWithoutTimezone(),
+            //             Email = "test-simple-user@hairun-technology.com",
+            //             Password = password,
+            //             IdPost = post.Id,
+            //             IdFile = file.Id,
+            //             CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //             UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //         };
+            //         await userRepository.AddAsync(newUser);
+            //         await context.SaveChangesAsync();
 
-                }
+            //     }
 
-                var departmentRepositoryAnonym = context.Departments;
-                var departementAnonyme = await departmentRepositoryAnonym.FirstOrDefaultAsync(da => da.Name == "Anonyme");
+            //     var departmentRepositoryAnonym = context.Departments;
+            //     var departementAnonyme = await departmentRepositoryAnonym.FirstOrDefaultAsync(da => da.Name == "Anonyme");
 
-                if (departementAnonyme == null)
-                {
-                    var roleRepository = context.Roles;
-                    var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Employé");
-                    departementAnonyme = new Department
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = "Anonyme",
-                        IdRoleNavigation = role,
-                        CreatedAt = GetDateTimeNowWithoutTimezone(),
-                        UpdatedAt = GetDateTimeNowWithoutTimezone()
-                    };
-                    await departmentRepositoryAnonym.AddAsync(departementAnonyme);
-                    await context.SaveChangesAsync();
-                }
+            //     if (departementAnonyme == null)
+            //     {
+            //         var roleRepository = context.Roles;
+            //         var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Employé");
+            //         departementAnonyme = new Department
+            //         {
+            //             Id = Guid.NewGuid(),
+            //             Name = "Anonyme",
+            //             IdRoleNavigation = role,
+            //             CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //             UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //         };
+            //         await departmentRepositoryAnonym.AddAsync(departementAnonyme);
+            //         await context.SaveChangesAsync();
+            //     }
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Une erreur est survenue lors du seeding des données: {ex.Message}");
-                throw;
-            }
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine($"Une erreur est survenue lors du seeding des données: {ex.Message}");
+            //     throw;
+            // }
         }
 
         private static async Task<Permission> GetOrCreatePermission(DbSet<Permission> repository, string permissionName)
--- Seeds/SuperAdminSeeder.cs
+++ Seeds/SuperAdminSeeder.cs
@@ -14,142 +14,142 @@ namespace back_dotnet.Seeds
     {
         public static async Task SeedAsync(HairunSiContext context)
         {
-            try
-            {
-                var jsonData = await System.IO.File.ReadAllTextAsync("seeds-id.json");
-                var seedIds = JsonSerializer.Deserialize<SeedData>(jsonData);
+            // try
+            // {
+            //     var jsonData = await System.IO.File.ReadAllTextAsync("seeds-id.json");
+            //     var seedIds = JsonSerializer.Deserialize<SeedData>(jsonData);
 
-                if (seedIds?.Id == null || !seedIds.Id.Any())
-                {
-                    Console.WriteLine($"Data from seeds-id.json: {jsonData}");
-                    throw new Exception("Les données dans le seeds-id.json son introuvables.");
-                }
+            //     if (seedIds?.Id == null || !seedIds.Id.Any())
+            //     {
+            //         Console.WriteLine($"Data from seeds-id.json: {jsonData}");
+            //         throw new Exception("Les données dans le seeds-id.json son introuvables.");
+            //     }
 
-                var superAdminId = Guid.Parse(seedIds.Id[0]);
+            //     var superAdminId = Guid.Parse(seedIds.Id[0]);
 
-                var userRepository = context.Users;
-                var user = await userRepository.FirstOrDefaultAsync(u => u.Email == "test@hairun-technology.com");
+            //     var userRepository = context.Users;
+            //     var user = await userRepository.FirstOrDefaultAsync(u => u.Email == "test@hairun-technology.com");
 
-                if (user == null)
-                {
-                    var permissionRepository = context.Permissions;
-                    var permissions = new[]
-                    {
-                    await GetOrCreatePermission(permissionRepository, "Accès tout utilisateur"),
-                    await GetOrCreatePermission(permissionRepository, "Création tout utilisateur"),
-                    await GetOrCreatePermission(permissionRepository, "Modification tout utilisateur"),
-                    await GetOrCreatePermission(permissionRepository, "Suppression tout utilisateur"),
-                    await GetOrCreatePermission(permissionRepository, "Validation demande congé")
-                };
+            //     if (user == null)
+            //     {
+            //         var permissionRepository = context.Permissions;
+            //         var permissions = new[]
+            //         {
+            //         await GetOrCreatePermission(permissionRepository, "Accès tout utilisateur"),
+            //         await GetOrCreatePermission(permissionRepository, "Création tout utilisateur"),
+            //         await GetOrCreatePermission(permissionRepository, "Modification tout utilisateur"),
+            //         await GetOrCreatePermission(permissionRepository, "Suppression tout utilisateur"),
+            //         await GetOrCreatePermission(permissionRepository, "Validation demande congé")
+            //     };
 
-                    var roleRepository = context.Roles;
-                    var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Super Admin");
+            //         var roleRepository = context.Roles;
+            //         var role = await roleRepository.FirstOrDefaultAsync(r => r.Name == "Super Admin");
 
-                    if (role == null)
-                    {
-                        role = new Role
-                        {
-                            Name = "Super Admin",
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            PermissionRoles = new List<PermissionRole>()
-                        };
+            //         if (role == null)
+            //         {
+            //             role = new Role
+            //             {
+            //                 Name = "Super Admin",
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 PermissionRoles = new List<PermissionRole>()
+            //             };
 
-                        foreach (var permission in permissions)
-                        {
-                            var permissionRole = new PermissionRole
-                            {
-                                IdPermission = permission.Id,
-                                IdRole = role.Id,
-                                CreatedAt = GetDateTimeNowWithoutTimezone(),
-                                UpdatedAt = GetDateTimeNowWithoutTimezone()
-                            };
+            //             foreach (var permission in permissions)
+            //             {
+            //                 var permissionRole = new PermissionRole
+            //                 {
+            //                     IdPermission = permission.Id,
+            //                     IdRole = role.Id,
+            //                     CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                     UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //                 };
 
-                            role.PermissionRoles.Add(permissionRole);
-                        }
+            //                 role.PermissionRoles.Add(permissionRole);
+            //             }
 
-                        await roleRepository.AddAsync(role);
-                        await context.SaveChangesAsync();
-                    }
+            //             await roleRepository.AddAsync(role);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var departmentRepository = context.Departments;
-                    var department = await departmentRepository.FirstOrDefaultAsync(d => d.Name == "Direction");
+            //         var departmentRepository = context.Departments;
+            //         var department = await departmentRepository.FirstOrDefaultAsync(d => d.Name == "Direction");
 
-                    if (department == null)
-                    {
-                        department = new Department
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Direction",
-                            IdRoleNavigation = role,
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            UpdatedAt = GetDateTimeNowWithoutTimezone()
-                        };
-                        await departmentRepository.AddAsync(department);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (department == null)
+            //         {
+            //             department = new Department
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "Direction",
+            //                 IdRoleNavigation = role,
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //             };
+            //             await departmentRepository.AddAsync(department);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var postRepository = context.Posts;
-                    var post = await postRepository.FirstOrDefaultAsync(p => p.Name == "Directeur");
+            //         var postRepository = context.Posts;
+            //         var post = await postRepository.FirstOrDefaultAsync(p => p.Name == "Directeur");
 
-                    if (post == null)
-                    {
-                        post = new Post
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "Directeur",
-                            IdDepartment = department.Id,
-                            CreatedAt = GetDateTimeNowWithoutTimezone(),
-                            UpdatedAt = GetDateTimeNowWithoutTimezone()
-                        };
-                        await postRepository.AddAsync(post);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (post == null)
+            //         {
+            //             post = new Post
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "Directeur",
+            //                 IdDepartment = department.Id,
+            //                 CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //                 UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //             };
+            //             await postRepository.AddAsync(post);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var fileRepository = context.Files;
-                    var file = await fileRepository.FirstOrDefaultAsync(f => f.Name == "hairun_admin");
+            //         var fileRepository = context.Files;
+            //         var file = await fileRepository.FirstOrDefaultAsync(f => f.Name == "hairun_admin");
 
-                    if (file == null)
-                    {
-                        file = new Models.Domain.File
-                        {
-                            Id = Guid.NewGuid(),
-                            Name = "hairun_admin",
-                            Path = "https://res.cloudinary.com/daeuzi9pb/image/upload/v1735561237/uploads/johndoe_rseb5e.webp",
-                            PublicId = "123456",
-                            Size = "160",
-                            Type = "png"
-                        };
-                        await fileRepository.AddAsync(file);
-                        await context.SaveChangesAsync();
-                    }
+            //         if (file == null)
+            //         {
+            //             file = new Models.Domain.File
+            //             {
+            //                 Id = Guid.NewGuid(),
+            //                 Name = "hairun_admin",
+            //                 Path = "https://res.cloudinary.com/daeuzi9pb/image/upload/v1735561237/uploads/johndoe_rseb5e.webp",
+            //                 PublicId = "123456",
+            //                 Size = "160",
+            //                 Type = "png"
+            //             };
+            //             await fileRepository.AddAsync(file);
+            //             await context.SaveChangesAsync();
+            //         }
 
-                    var password = BCrypt.Net.BCrypt.HashPassword("hairunM@2024*");
+            //         var password = BCrypt.Net.BCrypt.HashPassword("hairunM@2024*");
 
-                    var newUser = new User
-                    {
-                        Uuid = superAdminId,
-                        Firstname = "hairun",
-                        Lastname = "hairun",
-                        BirthDate = GetDateTimeNowWithoutTimezone(),
-                        Email = "test@hairun-technology.com",
-                        Password = password,
-                        IdPost = post.Id,
-                        IdFile = file.Id,
-                        CreatedAt = GetDateTimeNowWithoutTimezone(),
-                        UpdatedAt = GetDateTimeNowWithoutTimezone()
-                    };
+            //         var newUser = new User
+            //         {
+            //             Uuid = superAdminId,
+            //             Firstname = "hairun",
+            //             Lastname = "hairun",
+            //             BirthDate = GetDateTimeNowWithoutTimezone(),
+            //             Email = "test@hairun-technology.com",
+            //             Password = password,
+            //             IdPost = post.Id,
+            //             IdFile = file.Id,
+            //             CreatedAt = GetDateTimeNowWithoutTimezone(),
+            //             UpdatedAt = GetDateTimeNowWithoutTimezone()
+            //         };
 
-                    await userRepository.AddAsync(newUser);
-                    await context.SaveChangesAsync();
+            //         await userRepository.AddAsync(newUser);
+            //         await context.SaveChangesAsync();
 
-                    Console.WriteLine("Super Admin ajout avec succès en utilisant ID de seeds-id.json.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Une erreur est survenue lors du seeding des données: {ex.Message}");
-                throw;
-            }
+            //         Console.WriteLine("Super Admin ajout avec succès en utilisant ID de seeds-id.json.");
+            //     }
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine($"Une erreur est survenue lors du seeding des données: {ex.Message}");
+            //     throw;
+            // }
 
         }
 
